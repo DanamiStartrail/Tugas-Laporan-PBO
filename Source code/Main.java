737ce57ee824ace9d2a035b72410f3f5fa16f844
DanamiStartrail <mandantagurusinga@gmail.com>
2026-04-25 22:12:52 +0700

    Up tugas T4

--- Source code/Main.java
+++ Source code/Main.java
@@ -1,79 +1,38 @@
-class Mahasiswa {
-    private String nama, nim, prodi;
-    private int semester = 1;
+class Hewan{
+    String nama;
+    int umur;
 
-    public void setSemester(int semester) {
-            if (semester < 1) {
-                System.out.println("Semester tidak valid!");
-            } else {
-                this.semester = semester;
-            }
-        }
-    public int getSemester() {return this.semester;}
-    Mahasiswa(String nama, String nim, String prodi) {
+    public Hewan(String nama, int umur) {
         this.nama = nama;
-        this.nim = nim;
-        this.prodi = prodi;
+        this.umur = umur;
     }
 
-    Mahasiswa(String nama, String nim, String prodi, int semester) {
-        this.nama = nama;
-        this.nim = nim;
-        this.prodi = prodi;
-        setSemester(semester);
-    }
-    public void setNama(String nama) {this.nama = nama;}
-    public void setNim(String nim) {this.nim = nim;}
-    public void setProdi(String prodi) {this.prodi = prodi;}
-
-    public String getNama() {return this.nama;}
-    public String getNim() {return this.nim;}
-    public String getProdi() {return this.prodi;}
-
-    void tampilkanInfo() {
-        System.out.println("Nama: " + this.nama);
-        System.out.println("NIM: " + this.nim);
-        System.out.println("Prodi: " + this.prodi);
-        System.out.println("Semester: " + this.semester);
+    public void makan() {
+        System.out.println(nama + " sedang makan.");
     }
 
-    
-    
+    public void tidur() {
+        System.out.println(nama + " sedang tidur.");
+    }
 }
 
-class MataKuliah {
-    private String kodeMK;
-    private String namaMK;
-    private int sks;
-
-    public MataKuliah(String kodeMK, String namaMK, int sks) {
-        this.kodeMK = kodeMK;
-        this.namaMK = namaMK;
-        this.sks = sks;
+class Kucing extends Hewan {
+    public Kucing(String nama, int umur) {
+        super(nama, umur);
     }
-
-    public String getKodeMK() { return kodeMK; }
-    public void setKodeMK(String kodeMK) { this.kodeMK = kodeMK; }
-
-    public String getNamaMK() { return namaMK; }
-    public void setNamaMK(String namaMK) { this.namaMK = namaMK; }
-
-    public int getSks() { return sks; }
-    public void setSks(int sks) { this.sks = sks; }
 }
+
+
 
 public class Main {
     public static void main(String[] args) {
-        Mahasiswa mhs1 = new Mahasiswa("Mandanta", "121140147", "Teknik Informatika", 4);
-        Mahasiswa mhs2 = new Mahasiswa("Budi Utomo", "121140001", "Teknik Elektro", 2);
-        Mahasiswa mhs3 = new Mahasiswa("Siti Aminah", "121140099", "Sains Data", 0);
-        
-        System.out.println("Informasi Mahasiswa:");
-        mhs1.tampilkanInfo();
-        mhs2.tampilkanInfo();
-        mhs3.tampilkanInfo();
-        
-        MataKuliah mk1 = new MataKuliah("IF2123", "Struktur Data", 3);
-        System.out.println("Mata Kuliah: " + mk1.getNamaMK() + " (" + mk1.getSks() + " SKS)");
+        Hewan kucing = new Hewan("Kucing", 3);
+        Hewan anjing = new Hewan("Anjing", 5);
+
+        kucing.makan();
+        kucing.tidur();
+
+        anjing.makan();
+        anjing.tidur();
     }
 }
